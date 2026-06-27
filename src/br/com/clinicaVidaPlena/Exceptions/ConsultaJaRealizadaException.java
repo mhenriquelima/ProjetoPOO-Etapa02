3054f90f616e67aa72a286f25e1283bbf4b4f172
mhenriquelima <mhenriquedelima1@gmail.com>
2026-06-27 15:46:17 -0300

    fix: metodo override nas exceptions

--- src/br/com/clinicaVidaPlena/Exceptions/ConsultaJaRealizadaException.java
+++ src/br/com/clinicaVidaPlena/Exceptions/ConsultaJaRealizadaException.java
@@ -7,6 +7,7 @@ public class ConsultaJaRealizadaException extends Exception {
     public ConsultaJaRealizadaException(String mensagem) {
         super(mensagem);
     }
+    @Override
     public String toString() {
         return "a seguinte excecao ocorreu: " + this.getClass().getName() + "\n"
              + "Mensagem: " + this.getMessage() + "\n";
--- src/br/com/clinicaVidaPlena/Exceptions/PacienteInativoException.java
+++ src/br/com/clinicaVidaPlena/Exceptions/PacienteInativoException.java
@@ -7,6 +7,8 @@ public class PacienteInativoException extends Exception {
     public PacienteInativoException(String mensagem) {
         super(mensagem);
     }
+
+    @Override
     public String toString() {
         return "a seguinte excecao ocorreu: " + this.getClass().getName() + "\n"
              + "Mensagem: " + this.getMessage() + "\n";
